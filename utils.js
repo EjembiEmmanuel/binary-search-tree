d63996c69d39f321411275d652c491edfbbaaabb
EjembiEmmanuel <ejembioche50@gmail.com>
2023-07-13 05:33:14 +0100

    Refactored buildTree() Added prettyPrint() which visualizes the binary search tree

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 import Node from "./Node.js";
 
-function buildTree(array) {
+const buildTree = (array) => {
   array = array.sort();
 
   var start = 0;
@@ -16,6 +16,21 @@ function buildTree(array) {
   node.right = buildTree(array.slice(mid + 1));
 
   return node;
-}
+};
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(buildTree([3, 5, 1, 4, 2, 6, 7, 9]));
 
 export { buildTree };
