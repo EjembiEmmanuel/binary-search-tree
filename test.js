9e3a098006a233178083c9bb8990590978c7bb68
EjembiEmmanuel <ejembioche50@gmail.com>
2023-08-10 07:34:31 +0100

    Created height() function and refactored code
    height() finds and returns the height of a given node
    Replaced console.log() statements with return statements in some Tree() methods

--- test.js
+++ test.js
@@ -3,21 +3,23 @@ import { prettyPrint } from "./utils.js";
 
 (function main() {
   const tree = new Tree([]);
-  prettyPrint(tree.root);
 
   tree.insert(6);
   tree.insert(5);
   tree.insert(7);
   tree.insert(8);
   tree.insert(9);
+  tree.insert(4);
+  tree.insert(3);
 
   tree.delete(7);
   prettyPrint(tree.root);
-  tree.find(8);
 
-  tree.levelOrder();
-  tree.inOrder();
-  tree.preOrder();
-  tree.postOrder();
-  tree.depth();
+  console.log("Found Node:", tree.find(8));
+  console.log("Level order:", tree.levelOrder());
+  console.log("In order:", tree.inOrder());
+  console.log("Pre order:", tree.preOrder());
+  console.log("Post order:", tree.postOrder());
+  console.log("Depth:", tree.depth());
+  console.log("Height:", tree.height(5));
 })();
